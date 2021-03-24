e4ddee44abd7c4a094d93595db01266cb163c09d
necbal <61414566+necbal@users.noreply.github.com>
2021-03-24 16:13:35 +1300

    test

--- pseudosim.m
+++ pseudosim.m
@@ -1,6 +1,6 @@
 function [ out ] = pseudosim( input_args )
 
-v=0;D=0.1; pp=0.4; alp=1.9;
+v=0; D=0.1; pp=0.4; alp=1.9;
 pseudo=@(s) 0.1*s.^alp+50*(exp(-pp*s)-1+pp*s)-v*s+D*s.^2; %+10*(exp(-2*pp*s)-1+2*pp*s)+70*(exp(-(1.5)*pp*s)-1+(1.5)*pp*s)
 nx=2000;
 dt=0.0005;
